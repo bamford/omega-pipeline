7c8494bde8cd61ae6e99eec5ca2f2b1a9d80744f
Steven Bamford <steven@stevenbamford.com>
2016-01-07 17:26:28 +0000

    Add additional scripts from original pipeline

---
+++ all_in_one.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env /home/ppztw1/py-tim/bin/python
+
+import cutting_catalogue_liners
+
+cutting_catalogue_liners.in_field(2)
+cutting_catalogue_liners.in_field(3)
+cutting_catalogue_liners.in_field(5)
+cutting_catalogue_liners.in_field(6)
+cutting_catalogue_liners.in_field(7)
+cutting_catalogue_liners.in_field(8)
+cutting_catalogue_liners.in_field(9)
+cutting_catalogue_liners.in_field(10)
+cutting_catalogue_liners.in_field(11)
+cutting_catalogue_liners.in_field(12)
+cutting_catalogue_liners.in_field(13)
+cutting_catalogue_liners.in_field(14)
+cutting_catalogue_liners.in_field(15)
+cutting_catalogue_liners.in_field(16)
+cutting_catalogue_liners.in_field(17)
+cutting_catalogue_liners.in_field(18)
+cutting_catalogue_liners.in_field(19)
+cutting_catalogue_liners.in_field(20)
+cutting_catalogue_liners.in_field(21)
+cutting_catalogue_liners.in_field(22)
---
+++ creating_single_catalogue_liners_nocut.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import math
+
+
+
+################################################################################   
+filename = 'all_catalogue_cut_two_lines_aper5_master_hst_liners_nocut.txt'
+
+f = np.loadtxt(filename,dtype='str')
+
+field_number, ID_5,Prob_posiew_5,prob_agn5,prob_sf5,prob_retired5,prob_weak_agn5,\
+cont_maxprob_5,cont_median_5,rel_error_cont_median_5,rel_error_cont_maxlik_5,\
+z_maxprob_5,z_median_5,rel_error_z_median_5,\
+fha_maxprob_5,fha_median_5,rel_error_fha_median_5,rel_error_fha_maxlik_5,lha_median_5,\
+fnii_maxprob_5,fnii_median_5,rel_error_fnii_median_5,rel_error_fnii_maxlik_5,\
+ewha_median_5,ewha_maxlik_5,error_ewha_median_5,error_ewha_maxlik_5,lineratio_median_5,lineratio_maxlik_5,error_lineratio_median_5,error_lineratio_maxlik_5,\
+chisq_5,twolines_5,oneline_5,\
+ID_total,Prob_posiew_total,prob_agn_total,prob_sf_total,prob_retiredtotal,prob_weak_agntotal,\
+cont_maxprob_total,cont_median_total,rel_error_cont_median_total,rel_error_cont_maxlik_total,\
+z_maxprob_total,z_median_total,rel_error_z_median_total,\
+fha_maxprob_total,fha_median_total,rel_error_fha_median_total,rel_error_fha_maxlik_total,lha_median_total,\
+fnii_maxprob_total,fnii_median_total,rel_error_fnii_median_total,rel_error_fnii_maxlik_total,\
+ewha_median_total,ewha_maxlik_total,error_ewha_median_total,error_ewha_maxlik_total,lineratio_median_total,lineratio_maxlik_total,error_lineratio_median_total,error_lineratio_maxlik_total,\
+chisq_total,twolines_total,oneline_total,\
+ST_A_IMAGE,ra,dec,Rmag,combo_flag,stages_flag,MC_z,logmass,logmass_cl,flux24,tir,tuv,tir_cl,tuv_cl,\
+sfr_det,sfr_lo,sfr_hi,sfr_det_cl,sfr_lo_cl,sfr_hi_cl,sed_type = f[:].T
+
+formato = ('%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s \
+                %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s \
+                %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s\
+		  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s') 
+
+good = np.where(ID_5.astype(float) > 0)[0]
+ID_5_good = ID_5[good].astype(float)
+ewha_median_5_good = ewha_median_5[good].astype(float)
+error_ewha_median_5_good = error_ewha_median_5[good].astype(float)
+Prob_posiew_5_good = Prob_posiew_5[good].astype(float)
+twolines_5_good = twolines_5[good].astype(float)
+MC_z_good = MC_z[good].astype(float)
+z_median_total_good = z_median_total[good].astype(float)
+
+fha_median_aper5_nonstring = fha_median_5[good].astype(float)
+rel_error_fha_median_aper5_nonstring = rel_error_fha_median_5[good].astype(float)
+
+f_good = f[good]
+
+f4 = open('single_all_catalogue_cut_two_lines_aper5_master_hst_liners_nocut.txt','w')
+f4.write('%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n'%('#','field_number', ' ID_5', 'Prob_posiew_5', 'prob_agn5', 'prob_sf5','prob_retired5','prob_weak_agn5','cont_maxprob_5', 'cont_median_5', 'rel_error_cont_median_5', 'rel_error_cont_maxlik_5','z_maxprob_5', 'z_median_5', 'rel_error_z_median_5','fha_maxprob_5', 'fha_median_5', 'rel_error_fha_median_5', 'rel_error_fha_maxlik_5', 'lha_median_5','fnii_maxprob_5', 'fnii_median_5', 'rel_error_fnii_median_5', 'rel_error_fnii_maxlik_5','ewha_median_5', 'ewha_maxlik_5', 'error_ewha_median_5', 'error_ewha_maxlik_5', 'lineratio_median_5', 'lineratio_maxlik_5', 'error_lineratio_median_5', 'error_lineratio_maxlik_5','chisq_5', 'twolines_5', 'oneline_5','ID_total', 'Prob_posiew_total', 'prob_agn_total', 'prob_sf_total','prob_retiredtotal','prob_weak_agntotal','cont_maxprob_total', 'cont_median_total', 'rel_error_cont_median_total', 'rel_error_cont_maxlik_total','z_maxprob_total', 'z_median_total', 'rel_error_z_median_total','fha_maxprob_total', 'fha_median_total', 'rel_error_fha_median_total', 'rel_error_fha_maxlik_total', 'lha_median_total','fnii_maxprob_total', 'fnii_median_total', 'rel_error_fnii_median_total', 'rel_error_fnii_maxlik_total','ewha_median_total', 'ewha_maxlik_total', 'error_ewha_median_total', 'error_ewha_maxlik_total', 'lineratio_median_total', 'lineratio_maxlik_total', 'error_lineratio_median_total', 'error_lineratio_maxlik_total','chisq_total','twolines_total','oneline_total','ST_A_IMAGE', 'ra', 'dec', 'Rmag', 'combo_flag', 'stages_flag', 'MC_z', 'logmass', 'logmass_cl', 'flux24', 'tir', 'tuv','tir_cl','tuv_cl','sfr_det', 'sfr_lo', 'sfr_hi', 'sfr_det_cl', 'sfr_lo_cl', 'sfr_hi_cl', 'sed_type')) 
+f4.close()
+
+for ii in range(0,len(ID_5_good)):
+    how_many2 = np.where((np.isnan(MC_z_good) == False) & ((MC_z_good - z_median_total_good) <= (0.888*MC_z_good - 0.1358))  & (MC_z_good > 0.133) & (MC_z_good <= 0.195) & (ID_5_good == ID_5_good[ii]) & (error_ewha_median_5_good/(ewha_median_5_good) < 0.15) & (Prob_posiew_5_good  > 99.7) & (twolines_5_good == 1))[0]
+  
+    how_many = np.where(ID_5_good.astype(float) == ID_5_good[ii].astype(float))[0]
+
+    if len(how_many) == 1:
+        f_handle = file('single_all_catalogue_cut_two_lines_aper5_master_hst_liners_nocut.txt','a')
+        np.savetxt(f_handle,f_good[ii].reshape(1,88),fmt=formato)
+        f_handle.close()
+        
+    elif len(how_many) > 1:    
+        
+        if len(how_many2) == 0:
+	    
+            all_fluxes = fha_median_aper5_nonstring[how_many]*rel_error_fha_median_aper5_nonstring[how_many]
+
+            if fha_median_aper5_nonstring[ii]*rel_error_fha_median_aper5_nonstring[ii] == all_fluxes.min():
+                f_handle = file('single_all_catalogue_cut_two_lines_aper5_master_hst_liners_nocut.txt','a')
+                np.savetxt(f_handle,f_good[ii].reshape(1,88),fmt=formato)
+                f_handle.close()
+        
+        elif len(how_many2) > 0:
+              
+            all_fluxes = fha_median_aper5_nonstring[how_many2]*rel_error_fha_median_aper5_nonstring[how_many2]
+
+            if fha_median_aper5_nonstring[ii]*rel_error_fha_median_aper5_nonstring[ii] == all_fluxes.min():
+                f_handle = file('single_all_catalogue_cut_two_lines_aper5_master_hst_liners_nocut.txt','a')
+                np.savetxt(f_handle,f_good[ii].reshape(1,88),fmt=formato)
+                f_handle.close()
+
+
+
+################################################################################  
+filename = 'all_catalogue_cut_one_line_total_master_hst_liners_nocut.txt'
+
+f = np.loadtxt(filename,dtype='str')
+
+field_number, ID_5,Prob_posiew_5,prob_agn5,prob_sf5,prob_retired5,prob_weak_agn5,\
+cont_maxprob_5,cont_median_5,rel_error_cont_median_5,rel_error_cont_maxlik_5,\
+z_maxprob_5,z_median_5,rel_error_z_median_5,\
+fha_maxprob_5,fha_median_5,rel_error_fha_median_5,rel_error_fha_maxlik_5,lha_median_5,\
+fnii_maxprob_5,fnii_median_5,rel_error_fnii_median_5,rel_error_fnii_maxlik_5,\
+ewha_median_5,ewha_maxlik_5,error_ewha_median_5,error_ewha_maxlik_5,lineratio_median_5,lineratio_maxlik_5,error_lineratio_median_5,error_lineratio_maxlik_5,\
+chisq_5,twolines_5,oneline_5,\
+ID_total,Prob_posiew_total,prob_agn_total,prob_sf_total,prob_retiredtotal,prob_weak_agntotal,\
+cont_maxprob_total,cont_median_total,rel_error_cont_median_total,rel_error_cont_maxlik_total,\
+z_maxprob_total,z_median_total,rel_error_z_median_total,\
+fha_maxprob_total,fha_median_total,rel_error_fha_median_total,rel_error_fha_maxlik_total,lha_median_total,\
+fnii_maxprob_total,fnii_median_total,rel_error_fnii_median_total,rel_error_fnii_maxlik_total,\
+ewha_median_total,ewha_maxlik_total,error_ewha_median_total,error_ewha_maxlik_total,lineratio_median_total,lineratio_maxlik_total,error_lineratio_median_total,error_lineratio_maxlik_total,\
+chisq_total,twolines_total,oneline_total,\
+ST_A_IMAGE,ra,dec,Rmag,combo_flag,stages_flag,MC_z,logmass,logmass_cl,flux24,tir,tuv,tir_cl,tuv_cl,\
+sfr_det,sfr_lo,sfr_hi,sfr_det_cl,sfr_lo_cl,sfr_hi_cl,sed_type = f[:].T
+
+formato = ('%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s \
+                %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s \
+                %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s\
+		  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s') 
+
+
+f4 = open('single_all_catalogue_cut_one_line_total_aper_master_hst_liners_nocut.txt','w')
+f4.write('%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s  %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n'%('#','field_number', ' ID_5', 'Prob_posiew_5', 'prob_agn5', 'prob_sf5','prob_retired5','prob_weak_agn5','cont_maxprob_5', 'cont_median_5', 'rel_error_cont_median_5', 'rel_error_cont_maxlik_5','z_maxprob_5', 'z_median_5', 'rel_error_z_median_5','fha_maxprob_5', 'fha_median_5', 'rel_error_fha_median_5', 'rel_error_fha_maxlik_5', 'lha_median_5','fnii_maxprob_5', 'fnii_median_5', 'rel_error_fnii_median_5', 'rel_error_fnii_maxlik_5','ewha_median_5', 'ewha_maxlik_5', 'error_ewha_median_5', 'error_ewha_maxlik_5', 'lineratio_median_5', 'lineratio_maxlik_5', 'error_lineratio_median_5', 'error_lineratio_maxlik_5','chisq_5', 'twolines_5', 'oneline_5','ID_total', 'Prob_posiew_total', 'prob_agn_total', 'prob_sf_total','prob_retiredtotal','prob_weak_agntotal','cont_maxprob_total', 'cont_median_total', 'rel_error_cont_median_total', 'rel_error_cont_maxlik_total','z_maxprob_total', 'z_median_total', 'rel_error_z_median_total','fha_maxprob_total', 'fha_median_total', 'rel_error_fha_median_total', 'rel_error_fha_maxlik_total', 'lha_median_total','fnii_maxprob_total', 'fnii_median_total', 'rel_error_fnii_median_total', 'rel_error_fnii_maxlik_total','ewha_median_total', 'ewha_maxlik_total', 'error_ewha_median_total', 'error_ewha_maxlik_total', 'lineratio_median_total', 'lineratio_maxlik_total', 'error_lineratio_median_total', 'error_lineratio_maxlik_total','chisq_total','twolines_total','oneline_total','ST_A_IMAGE', 'ra', 'dec', 'Rmag', 'combo_flag', 'stages_flag', 'MC_z', 'logmass', 'logmass_cl', 'flux24', 'tir', 'tuv','tir_cl','tuv_cl','sfr_det', 'sfr_lo', 'sfr_hi', 'sfr_det_cl', 'sfr_lo_cl', 'sfr_hi_cl', 'sed_type')) 
+f4.close()
+
+
+galaxies_taken = []
+
+fha_median_total_nonstring = fha_median_total.astype(float)
+rel_error_fha_median_total_nonstring = rel_error_fha_median_total.astype(float)
+
+for ii in range(0,len(ID_total)):
+
+    how_many2 = np.where((np.isnan(MC_z.astype(float)) == False) & ((MC_z.astype(float) - z_median_total.astype(float)) <= (0.888*MC_z.astype(float) - 0.1358))  & (MC_z.astype(float) > 0.133) & (MC_z.astype(float) <= 0.195) & (ID_total.astype(float) == ID_total[ii].astype(float)) & ((error_ewha_median_total.astype(float)/(ewha_median_total.astype(float)) < 0.15) & (Prob_posiew_total.astype(float)  > 99.7) & (oneline_total.astype(float) == 1)))[0]
+  
+    how_many = np.where(ID_total.astype(float) == ID_total[ii].astype(float))[0]
+
+    if len(how_many) == 1:
+        f_handle = file('single_all_catalogue_cut_one_line_total_aper_master_hst_liners_nocut.txt','a')
+        np.savetxt(f_handle,f[ii].reshape(1,88),fmt=formato)
+        f_handle.close()
+        
+    
+    elif len(how_many) > 1:    
+        
+        if len(how_many2) == 0:
+	    
+            all_fluxes = fha_median_total_nonstring[how_many]*rel_error_fha_median_total_nonstring[how_many]
+
+            if fha_median_total_nonstring[ii]*rel_error_fha_median_total_nonstring[ii] == all_fluxes.min():
+                f_handle = file('single_all_catalogue_cut_one_line_total_aper_master_hst_liners_nocut.txt','a')
+                np.savetxt(f_handle,f[ii].reshape(1,88),fmt=formato)
+                f_handle.close()
+        
+        elif len(how_many2) > 0:
+              
+            all_fluxes = fha_median_total_nonstring[how_many2]*rel_error_fha_median_total_nonstring[how_many2]
+
+            if fha_median_total_nonstring[ii]*rel_error_fha_median_total_nonstring[ii] == all_fluxes.min():
+                f_handle = file('single_all_catalogue_cut_one_line_total_aper_master_hst_liners_nocut.txt','a')
+                np.savetxt(f_handle,f[ii].reshape(1,88),fmt=formato)
+                f_handle.close()
